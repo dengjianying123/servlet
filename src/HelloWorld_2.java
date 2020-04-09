a48988875f942fbd2cdcec9623943e9d5cfc0769
dengjianying123 <dengjianying_xny@163.com>
2020-04-09 14:43:15 +0800

    補充

---
+++ src/HelloWorld_2.java
@@ -0,0 +1,10 @@
+public class HelloWorld_2 {
+	
+	public static void main(String[] args) {
+		System.out.println("999");
+		System.out.println("1111");
+		System.out.println("3333");
+		System.out.println("hello fy");
+	}
+
+}
